d433a59bba1aa350b5a2bb0c18b29db543ba5c62
taetaerin <taerin13@naver.com>
2023-12-21 12:38:01 +0900

    refactor: users 파일

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,16 @@ const express = require('express')
 const router = express.Router()
 const conn = require('../mariadb');
 
-// JSON 형태의 요청(request) body를 파싱하기 위해 사용되는 미들웨어
 router.use(express.json())
 
-//로그인
 router.post('/login', function(req, res) {
-    //userId가 db에 저장된 회원인지 확인
-    let {email, password} = req.body
-
-    conn.query(
-        `SELECT * FROM users WHERE email = ?`, email,
+    const {email, password} = req.body
+    let sql = `SELECT * FROM users WHERE email = ?`
+    
+    conn.query(sql, email,
         function(err, results) {
             let loginUser = results[0]
-    
+            
             if (loginUser && loginUser.password == password) {
                 res.status(200).json({
                     message: `${loginUser.name}님 로그인 되었습니다.`
@@ -30,7 +27,6 @@ router.post('/login', function(req, res) {
 })
 
 
-//회원가입
 router.post('/join', function(req, res) {
     if(req.body == {}) {
         res.status(400).json({
@@ -39,10 +35,10 @@ router.post('/join', function(req, res) {
     } 
     else {
         const {email, name, password, contact} = req.body
-
-        conn.query(
-            `INSERT INTO users (email, name, password, contact) 
-                VALUES (?, ?, ?, ?)`, [email, name, password, contact],
+        
+        let sql = `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`
+        let values = [email, name, password, contact]
+        conn.query(sql, values,
             function(err, results) {
                 res.status(201).json(results)
             }
@@ -52,23 +48,21 @@ router.post('/join', function(req, res) {
 
 router
     .route('/users')
-    //개별 조회
     .get(function(req, res) {
         let {email} = req.body
-
-        conn.query(
-            `SELECT * FROM users WHERE email = ?`, email,
+        
+        let sql = `SELECT * FROM users WHERE email = ?`
+        conn.query(sql, email,
             function(err, results) {
                 res.status(200).json(results)
             }
         );  
     })
-    //개별 탈퇴
     .delete(function(req, res) {
         let {email} = req.body;
         
-        conn.query(
-            `DELETE FROM users WHERE email = ?`, email,
+        let sql = `DELETE FROM users WHERE email = ?`
+        conn.query(sql, email,
             function(err, results) {
                 res.status(200).json(results)
             }
